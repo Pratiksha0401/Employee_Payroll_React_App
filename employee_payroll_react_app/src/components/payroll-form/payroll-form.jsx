310cb4029845e4e7d36bb5be21ff09fdc8b47c84
Pratiksha0401 <nagoshepratiksha@gmail.com>
2021-07-08 14:17:39 +0530

    Added: code to connecting react js frontend with spring boot apis

--- employee_payroll_react_app/src/components/payroll-form/payroll-form.jsx
+++ employee_payroll_react_app/src/components/payroll-form/payroll-form.jsx
@@ -291,15 +291,15 @@ const PayrollForm = () => {
                         <div>
                             <select onChange={changeValue} id="day" name="day" value={formValue.day} required>
                                 <option value="">Day</option>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
-                                <option value="9">9</option>
+                                <option value="01">01</option>
+                                <option value="02">02</option>
+                                <option value="03">03</option>
+                                <option value="04">04</option>
+                                <option value="05">05</option>
+                                <option value="06">06</option>
+                                <option value="07">07</option>
+                                <option value="08">08</option>
+                                <option value="09">09</option>
                                 <option value="10">10</option>
                                 <option value="11">11</option>
                                 <option value="12">12</option>
@@ -336,7 +336,7 @@ const PayrollForm = () => {
                                 <option value="Sep">Sept</option>
                                 <option value="Oct">Oct</option>
                                 <option value="Nov">Nov</option>
-                                <option value="Dec">dec</option>
+                                <option value="Dec">Dec</option>
                             </select>
                             <select onChange={changeValue} id="year" name="year" value={formValue.year} required>
                                 <option value="">Year</option>
